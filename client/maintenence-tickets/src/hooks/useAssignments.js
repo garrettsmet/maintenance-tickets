02387b41bd682fe4dbdfd112db2212c374f387a1
Garrett Smetana <garrettsmet@yahoo.com>
2023-06-27 12:31:21 -0600

    restore mistakenly deleted work

--- client/maintenence-tickets/src/hooks/useAssignments.js
+++ client/maintenence-tickets/src/hooks/useAssignments.js
@@ -4,11 +4,31 @@ import { getAssignments } from "../services/dataService";
 export function useAssignments() {
 	const [assignments, setAssignments] = useState([]);
 
+	const context = { assignments };
+
 	useEffect(() => {
 		getAssignments().then((res) => {
 			setAssignments(res);
 		});
 	}, []);
 
-	return { assignments, setAssignments };
+	const appendActions = {
+		isAssigned: (ticket_id, worker_id) =>
+			isAssigned(ticket_id, worker_id, context),
+	};
+
+	return { assignments, setAssignments, appendActions };
+}
+
+function isAssigned(ticket_id, worker_id, context) {
+	const { assignments } = context;
+
+	for (let i = 0; i < assignments.length; i++) {
+		if (
+			assignments[i].ticket_id === ticket_id &&
+			assignments[i].worker_id === worker_id
+		) {
+			return true;
+		}
+	}
 }
